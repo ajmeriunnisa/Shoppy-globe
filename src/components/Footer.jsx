c89c8411de2d811902181e809a1eac6165acbc49
ajmeriunnisa <ajmeriunnisa877@gmail.com>
2025-11-05 12:39:35 +0530

    Added Styling to Footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React from 'react';
+import { FaLinkedin, FaGithub } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 function Footer() {
-  return (
-    <div>Footer</div>
-  )
+    return (
+        // ===== Footer Section =====
+        <footer className="fixed bottom-0 left-0 w-full bg-gray-200 mt-12 py-6 text-center border-t border-gray-200">
+
+            {/* ===== Copyright Text ===== */}
+            <p className="text-gray-600 text-sm">
+                © {new Date().getFullYear()} <Link to='/'><span className="cursor-pointer font-semibold">ShoppyGlobe</span></Link>. All rights reserved.
+            </p>
+
+            {/* ===== Social Media Links (LinkedIn and GitHub) ===== */}
+            <div className="flex justify-center items-center gap-6 mt-3">
+              <p className="text-gray-600">Contact Me - </p>
+                <a
+                    href="https://www.linkedin.com/in/ajmeri-unnisa/"
+                    target="_blank"
+                    className="text-gray-600 hover:text-blue-600 transition"
+                >
+                    <FaLinkedin size={22} />
+                </a>
+
+                <a
+                    href="https://github.com/ajmeriunnisa"
+                    target="_blank"
+
+                    className="text-gray-600 hover:text-black transition"
+                >
+                    <FaGithub size={22} />
+                </a>
+            </div>
+
+            {/* ===== Creator Info ===== */}
+            <p className="text-gray-600 text-xs mt-3">
+                Created by <span className="font-medium">Ajmeri Unnisa</span> for React Project.
+            </p>
+        </footer>
+    );
 }
 
-export default Footer
+export default Footer;
